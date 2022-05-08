9848e844cd5f9121e20032bd8f8c6762c0574cfe
Lucas Thompson <doctorthompson@gmail.com>
2022-05-07 21:00:34 -0600

    Added MOSFET for controlling power to the LED strip Trimmed LED count to 83

--- LED_Larson_Scanner.ino
+++ LED_Larson_Scanner.ino
@@ -1,17 +1,22 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 87               // How many leds in your strip?
-#define DATA_PIN 2                // Data pin D4 on NodeMCU
-#define COLOR 0xcc3300            // Pulse colour
-#define MAX_POWER_VOLTS     12    // Voltage
-#define MAX_POWER_MILLIAMPS 4000  // Milliamps MAX
+#define NUM_LEDS 83               // How many leds in your strip?
+#define DATA_PIN 2                // (D4) Data out to strip
+#define POWER_PIN 5               // (D1) MOSFET control 12V power to strip
+#define COLOR 0xCC3300            // Pulse colour
+#define MAX_POWER_VOLTS     12    // Automotive voltage
+#define MAX_POWER_MILLIAMPS 4000  // Milliamp max (Should match inline fuse)
 #define BRIGHTNESS 240            // LED Brightness (0-255)
 #define BEATS 36                  // BPM of full cycle
-
+#define DECAY 18                  // How fast pulse fades
 CRGBArray<NUM_LEDS> leds;         // LED memory addresses
 uint16_t lead_dot = 0;            // Current Pulse Position
 
 void setup() {
+  // Power up the strip
+  analogWrite(POWER_PIN,0);
+  delay(300);
+  analogWrite(POWER_PIN,1023);
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setCorrection( TypicalLEDStrip );
   FastLED.setMaxPowerInVoltsAndMilliamps( MAX_POWER_VOLTS, MAX_POWER_MILLIAMPS);
@@ -23,9 +28,10 @@ void setup() {
 
 void loop() {
   EVERY_N_MILLISECONDS(4) {
+    leds.fadeToBlackBy(DECAY);
     lead_dot = beatsin16( BEATS, 0, NUM_LEDS-1 );
     leds[lead_dot] = COLOR;
     FastLED.show();
-    leds.fadeToBlackBy(18);
+    
   }
 }
